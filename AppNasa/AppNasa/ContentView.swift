160f5dd5487c57349392627829c6d860c5afd8e9
Christopher Pedraza Pohlenz <62347713+christopher-pedraza@users.noreply.github.com>
2023-08-14 11:22:55 -0600

    Add files via upload

--- AppNasa/AppNasa/ContentView.swift
+++ AppNasa/AppNasa/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var PhotoVM = PhotoViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        // Para cada uno de los elementos dentro del
+        // del arreglo, lo guardamos en item
+        List(PhotoVM.arrPhoto) { item in
+            VStack {
+                Text(item.title)
+                // Con async image podemos ir descargando las imagenes
+                // mientras las vamos necesitamos aunque no las tengamos
+                // en assets
+                AsyncImage(url: URL(string: item.url))
+                    .scaledToFit()
+                    .frame(height: 150)
+                    .clipped()
+            }
+            
+            
         }
-        .padding()
+        .task {
+            do {
+                try await PhotoVM.getPhotosData()
+            } catch {
+                print("Error")
+            }
+        }
     }
 }
 
--- AppNasa/AppNasa/PhotoViewModel.swift
+++ AppNasa/AppNasa/PhotoViewModel.swift
@@ -16,12 +16,14 @@ class PhotoViewModel : ObservableObject {
     
     // Como vamos a hacer un request, queremos que nuestra funcion se espere en lo que recibe los datos
     // Para esto, hacemos que la funcion sea async
-    func getPhotosData() async {
+    // Adicionalmente, para manejar los errores del try, ponemos throws
+    func getPhotosData() async throws {
         // urlrequest prepara para hacer un request
         // urlsession ya hace el request
         // jsondecoder decodifica el json que obtuvimos
         
         // Guard es como un trycase, si se logra crear la variable url continua, sino, muestra error
+        // Obtener el objeto URL
         guard let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=10")
             else {
                 // Si no lo logra, muestra error y sale de la funcion
@@ -34,9 +36,25 @@ class PhotoViewModel : ObservableObject {
         let urlRequest = URLRequest(url: url)
         
         // Agregamops el try para manejar errores y el await para que el programa se espere a recibir la respuesta
+        // Realizamos la llamada al urlRequest obteniendo datos
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
         
-        // Video 28:57
+        // Checar si se obtuvo una respuesta o no
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            print("HTTP Request Failed")
+            return
+        }
         
+        // Hacemos un decode del JSON en un arreglo de PhotoModel de data
+        // Decodificar la informacion de formato JSON
+        let results = try JSONDecoder().decode([PhotoModel].self, from: data)
+        
+        // Llenamos el arreglo que es publico con los datos del JSON
+        // Para no ejecutarlo en el background sino en el thread principal
+        // Utilizamos el thread principal para actualizar la variable arrPhoto
+        DispatchQueue.main.async {
+            // Usamos el self para referirnos que buscamos dentro de esta clase
+            self.arrPhoto = results
+        }
     }
 }
